12687d6e0898b7cd360221b207626bfcd09e92ea
Rob Blasetti <rblasett@thoughtworks.com>
2014-03-12 15:33:52 +1100

    initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: user
+ * Date: 12/03/2014
+ * Time: 3:16 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+public class Main {
+
+}
